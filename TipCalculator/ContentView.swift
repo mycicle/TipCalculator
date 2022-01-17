23c0a5f9c505cde365e56a9a8ee4870f701404bc
Mycicle <mjm.digregorio@gmail.com>
2022-01-17 14:52:29 -0500

    iOS app which helps you calculate cost per person
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -9,80 +9,89 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    @State private var checkAmount: Double? = nil
+    @State private var tipPercentage: Int = 20
+    @State private var numberOfPeople: Int = 3
+    @FocusState private var checkAmountIsFocused: Bool
+    
+    private let tipPercentages: [Int] = [10, 15, 20, 25, 0]
+    
+    var totalBillAmount: Double {
+        let tipSelection = Double(tipPercentage)
+        guard let totalAmount = checkAmount else { return 0.0 }
+            
+        let tipPercent: Double = 1 + (tipSelection / 100)
+        
+        return totalAmount * tipPercent
+    }
+    
+    var amountPerPerson: Double {
+        
+        let numPeople = Double(numberOfPeople) + 1
+        return totalBillAmount / numPeople
+    }
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            Form {
+                Section {
+                    // Base amount and number of people
+                    TextField("Total Check Amount", value: $checkAmount, format: .currency( code: Locale.current.currencyCode ?? "USD" ) )
+                        .keyboardType(.decimalPad)
+                        .focused($checkAmountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(1..<100) {
+                            if ($0 == 1) {
+                                Text("\($0) Person")
+                            } else {
+                                Text("\($0) People")
+                            }
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
+                
+                Section {
+                    // tip selector
+                    Picker("Tip Amount", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much do you want to tip?")
+                }
+                
+                Section {
+                    Text(totalBillAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Total Bill Amount")
+                }
+                Section {
+                    // final calculation
+                    Text(amountPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Total Payment Per Person")
+                }
             }
+            .navigationTitle("Cost Calculator")
             .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+
+                    Button("Done") {
+                        checkAmountIsFocused = false
                     }
                 }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
